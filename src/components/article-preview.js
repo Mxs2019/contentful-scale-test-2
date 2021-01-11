12d504a2133abee4e560be03bd362083046f39c5
Max Shaw <Maxdshaw@gmail.com>
2021-01-10 21:41:15 -0500

    Make preview smaller

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -5,16 +5,10 @@ import Img from 'gatsby-image'
 import styles from './article-preview.module.css'
 
 export default ({ article }) => (
-  <div className={styles.preview}>
-    <Img alt="" fluid={article.heroImage.fluid} />
+  <div className={styles.preview}>    
     <h3 className={styles.previewTitle}>
       <Link to={`/blog/${article.slug}`}>{article.title}</Link>
     </h3>
-    <small>{article.publishDate}</small>
-    <p
-      dangerouslySetInnerHTML={{
-        __html: article.description.childMarkdownRemark.html,
-      }}
-    />
+    <small>{article.publishDate}</small>    
   </div>
 )
